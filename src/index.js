854bcd7eaa1563b39fab9e4ab413b5e18dc6d987
Osnova207 <osnova207@gmail.com>
2020-09-16 14:48:57 +0300

    FTR: link store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/styles.scss';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import reducer from "./reducers/index";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
 );
