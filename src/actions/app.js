d6dffc53bf979196187eba673913a01f99603124
Osnova207 <osnova207@gmail.com>
2020-09-16 14:48:17 +0300

    FTR: added actions

---
+++ src/actions/app.js
@@ -0,0 +1,11 @@
+export const openLoginModal = () => ({ type: 'OPEN_LOGIN_MODAL'});
+
+export const closeLoginModal = () => ({ type: 'CLOSE_LOGIN_MODAL'});
+
+export const openAddNewsModal = () => ({ type: 'OPEN_ADD_NEWS_MODAL'});
+
+export const closeAddNewsModal = () => ({ type: 'CLOSE_ADD_NEWS_MODAL'});
+
+export const setLogIn = (payload) => ({ type: 'SET_LOG_IN', payload});
+
+export const setLogOut = () => ({ type: 'SET_LOG_OUT'});
---
+++ src/actions/news.js
@@ -0,0 +1,5 @@
+export const toAddNews = (payload) => ({ type: 'ADD_NEWS', payload});
+
+export const toDeleteNews = (payload) => ({ type: 'DELETE_NEWS', payload});
+
+export const toApproveNews = (payload) => ({ type: 'APPROVE_NEWS', payload});
